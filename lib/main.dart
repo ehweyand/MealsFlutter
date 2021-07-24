03aded06d5d1a1e6ffea7b9c8c6a13940f531719
Evandro Henrique <ehweyand@universo.univates.br>
2021-07-24 17:24:18 -0300

    filters feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './models/meal.dart';
 import './screens/filters_screen.dart';
 import './screens/tabs_screen.dart';
-
 import './screens/meal_detail_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Filtros
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+
+  // Controla os Meals
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
+  void _setFilters(Map<String, bool> filterData) {
+    // Atualiza os filtros
+    setState(() {
+      _filters = filterData;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        // Lógica pra verificar os filtros
+        if (_filters['gluten'] && !meal.isGlutenFree) {
+          return false; //não quero incluir na busca
+        }
+
+        if (_filters['lactose'] && !meal.isLactoseFree) {
+          return false; //não quero incluir na busca
+        }
+
+        if (_filters['vegan'] && !meal.isVegan) {
+          return false; //não quero incluir na busca
+        }
+
+        if (_filters['vegetarian'] && !meal.isVegetarian) {
+          return false; //não quero incluir na busca
+        }
+
+        return true; // mantém a refeição na lista (do where)
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,9 +82,11 @@ class MyApp extends StatelessWidget {
       //initialRoute: '/', //default é '/'
       routes: {
         '/': (ctx) => TabsScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        CategoryMealsScreen.routeName: (ctx) =>
+            CategoryMealsScreen(_availableMeals),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-        FiltersScreen.routeName: (ctx) => FiltersScreen(),
+        //passando parâmetros para a rota (o ponteiro para a função)
+        FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters),
       },
       //se acessar uma rota não registrada em routes
       /*onGenerateRoute: (settings) {
--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/meal_item.dart';
-import '../dummy_data.dart';
 import '../models/meal.dart';
 
 class CategoryMealsScreen extends StatefulWidget {
   static const routeName = '/category-meals';
+
+  final List<Meal> availableMeals;
+
+  CategoryMealsScreen(this.availableMeals);
 
   @override
   _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
@@ -34,7 +37,7 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
           ModalRoute.of(context).settings.arguments as Map<String, String>;
       final categoryId = routeArgs['id'];
       categoryTitle = routeArgs['title'];
-      displayedMeals = DUMMY_MEALS.where((meal) {
+      displayedMeals = widget.availableMeals.where((meal) {
         return meal.categories.contains(categoryId);
       }).toList();
 
